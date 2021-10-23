3c5864ce18ed2989d5ede3a5cb301289ee91fbfb
jihye <wlgp1056@naver.com>
2021-10-23 17:30:03 +0900

    artfor_react_1023_1729

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Header from "././pages/Header";
-import Footer from "./pages/Footer";
+import Header from "././components/Header";
+import Footer from "././components/Footer";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import { Switch, Route } from "react-router-dom";
