3cddb0dbe06e8795c930cd5f0d90b22a23f98e7a
kjhgood3 <kjhgood3@naver.com>
2021-10-24 21:49:37 +0900

    artfor_react_1024_2149_settings

---
+++ src/pages/users/Project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Project() {
+  return (
+    <div>
+      <p>후원한 프로젝트가 없습니다.</p>   
+    </div>
+  );
+};
+
+export default Project;
