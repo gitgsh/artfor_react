38fd0c07735af1a1fbe4876261c5f1f0afd5db12
hyunahpark <pha5763@gmail.com>
2021-10-25 11:13:18 +0900

    G_List, List.css update

--- src/pages/guide/G_List_practice.js
+++ src/pages/guide/G_List_practice.js
@@ -1,85 +1,110 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Guide.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./List.css";
 
-function G_List_practice() {
+function G_List_practice(props) {
 
-    let [글제목, 글제목변경함수] = useState(['크라우드펀딩과 후원이란 무엇인가요?', '펀딩 수수료는 얼마인가요?', '지식재산권 침해 신고 가이드']);
+  let [제목, 제목변경함수] = useState(['아트포(artfor)는 무엇인가요?', '크라우드펀딩과 후원이란 무엇인가요?', '펀딩 수수료는 얼마인가요?', '지식재산권 침해 신고 가이드']);
 
+  let [modal, setModal] = useState(true);
+  let [title, setTitle] = useState(0);
 
-    let [modal, modal변경함수] = useState(false);
-    let [누른제목, 누른제목변경함수] = useState(0);
-    let [입력값, 입력값변경함수] = useState('');
-    
+  console.log(props.data);
 
+  useEffect(() => {
+    console.log(">>> G_list로 들어옴");
+    // componentDidMount
+    // 컴포넌트가 화면에 나타날 때 실행하고 싶은 함수를 이 곳에 넣는다.
 
-    // function 제목바꾸기() {
-    //   var newArray = [...글제목];
-    //   newArray[0] = '여자코트 추천';
-    //   글제목변경함수(newArray);
-    // }
+    axios
+      .get("http://localhost:8004/app/guide/list.do")
+      .then((result) => {
+        console.log("ajax 요청 성공함");
+        console.log("dd", result);
 
-    return (
-      <div className="container_guide">
-        <div className="black-nav">
-          <div>artfor 이용 안내</div>
-        </div>
-        {/* <div className="list">
-          <h3>{ 글제목[0] }</h3>
-          <p>2월 17일 발행</p>
-          <hr/>
-        </div> */}
+        let dataA = result.data.list2;
 
-        <div className="guide_list">
-          {
-            글제목.map(function(글, i) {
-              return (
-                <div className="list" key={i}>
-                <h3 onClick={ () => { 누른제목변경함수(i)} }><span Link to={ modal }>{ 글 }</span></h3>
-                <p>2월 18일 발행</p>
-              </div>
-              )
-            })
-          }
-        </div>
-          {/* <input onChange={ (e) => { 입력값변경함수(e.target.value) }} />
+        props.setData([...dataA]);
+      })
+      .catch((err) => {
+        console.log("dd", err);
+        console.log("ajax 요청 실패함", err);
+      });
+  }, []);
 
-
-          <div className="publish">
-            <input onChange={ (e) => { 입력값변경함수(e.target.value)} }/>
-            <button onClick={ () => {
-              var arrayCopy = [...글제목];
-              arrayCopy.unshift(입력값);
-              글제목변경함수(arrayCopy);
-            } }>저장하기</button> 
-          </div> */}
-         
-
-          {/* <button onClick={ ()=>{ 누른제목변경함수(0) } }>버튼1</button>
-          <button onClick={ ()=>{ 누른제목변경함수(1) } }>버튼2</button>
-          <button onClick={ ()=>{ 누른제목변경함수(2) } }>버튼3</button> */}
-          <button onClick={()=>{modal변경함수(!modal)}}>벝</button>
-
-        {/* <div className="guide_detail"> */}
-          {
-            modal === true
-            ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
-            : null
-          }
-        {/* </div> */}
-
-      </div>
-    );
-}
-
-function Modal(props) {
   return (
-    <div className="modal">
-      <h2>{props.글제목[props.누른제목]}</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
+    <div className="container-guide">
+      <h3 className="page-name">이용안내 게시판</h3>
+      <div>
+      <Link to="./G_Input">
+        <Button className="guide-writeBtn" variant="dark">
+          글쓰기
+        </Button>
+      </Link>{" "}
+      </div>
+      <div className="div-guide">
+      <div className="div-guide-list">
+          {/* <tbody>
+            {props.data.map((data, i) => {
+              return (
+                <div key={i}>
+                  <h3 onClick={ () => {
+                    setTitle(i)
+                  }} >{data.g_title}</h3>
+                  <p>{data.g_day} 발행</p>
+                </div>
+              );
+            })}
+          </tbody> */}
+          <div className="div-guide-list-1">
+            {
+              제목.map(function(글,i) {
+                return (
+                  <div key={i}>
+                    <div className="div-guide-list-1" onClick={()=>{
+                      setTitle(i)
+                    }}><Link to={modal}>{ 글 }</Link></div>
+                  </div>
+                )
+              })
+            }
+            </div>
+        </div>
+
+      <div className="guide-modal">
+            {/* {props.data.map((data, i) => {
+              return (
+                <div key={i}>
+                  <div>{data.g_no}</div>
+                  <div>{data.g_title}</div>
+                  <div>{data.g_writer}</div>
+                  <div>{data.g_content}</div>
+                  <div>{data.g_day}</div>
+                </div>
+              );
+            })}  */}
+            {
+              modal === true
+              ? <ModalPage 제목={제목} title={title}></ModalPage>
+              : null
+            }
+      </div>
+</div>
     </div>
-  )
+  );
 }
+
+  function ModalPage(props) {
+    return (
+      <div>
+        <h2>{props.제목[props.title]}</h2>
+        <p>날짜</p>
+        <p>내용</p>
+      </div>
+    )
+  }
+
 
 export default G_List_practice;
