33ecfd7a9ba924f4290b478e267bcba5b8c71b36
bitcoinbucket <googoojoa@gmail.com>
2021-10-25 21:57:45 +0900

    arfor_react_1025_2157

--- src/pages/guide/G_List.js
+++ src/pages/guide/G_List.js
@@ -5,9 +5,12 @@ import { Link } from "react-router-dom";
 import "./list.css";
 
 function List(props) {
-
-  let [제목, 제목변경] = useState(['아트포(artfor)는 무엇인가요?', '크라우드펀딩과 후원이란 무엇인가요?', '펀딩 수수료는 얼마인가요?', '지식재산권 침해 신고 가이드']);
-
+  let [제목, 제목변경] = useState([
+    "아트포(artfor)는 무엇인가요?",
+    "크라우드펀딩과 후원이란 무엇인가요?",
+    "펀딩 수수료는 얼마인가요?",
+    "지식재산권 침해 신고 가이드",
+  ]);
 
   let [modal, setModal] = useState(true);
   let [누른제목, 누른제목변경] = useState(0);
@@ -39,14 +42,14 @@ function List(props) {
     <div className="container-guide">
       <h3 className="page-name">이용안내 게시판</h3>
       <div>
-      <Link to="./G_Input">
-        <Button className="guide-writeBtn" variant="dark">
-          글쓰기
-        </Button>
-      </Link>{" "}
+        <Link to="./G_Input">
+          <Button className="guide-writeBtn" variant="dark">
+            글쓰기
+          </Button>
+        </Link>{" "}
       </div>
       <div className="div-guide">
-      <div className="div-guide-list">
+        <div className="div-guide-list">
           {/* <tbody>
             {props.data.map((data, i) => {
               return (
@@ -60,34 +63,39 @@ function List(props) {
             })}
           </tbody> */}
           <div className="div-guide-list-1">
-            {
-              제목.map(function(글,i) {
-                return (
-                  <div key={i}>
-                    <div className="div-guide-list-1" onClick={()=>{
-                      누른제목변경(i)
-                    }}><Link to={modal}>{ 글 }</Link></div>
+            {제목.map(function (글, i) {
+              return (
+                <div key={i}>
+                  <div
+                    className="div-guide-list-1"
+                    onClick={() => {
+                      누른제목변경(i);
+                    }}
+                  >
+                    <Link to={modal}>{글}</Link>
                   </div>
-                )
-              })
-            }
-            {
-              props.data.map((d, i) => {
-                return (
-                  <div key={i}>
-                    <h3 onClick={ () => {
-                      누른제목변경(i)
-                    }} >{d.g_title}</h3>
-                    <p>{d.g_day} 발행</p>
-                  </div>
-                );
-              })
-            }
-            </div>
+                </div>
+              );
+            })}
+            {props.data.map((d, i) => {
+              return (
+                <div key={i}>
+                  <h3
+                    onClick={() => {
+                      누른제목변경(i);
+                    }}
+                  >
+                    {d.g_title}
+                  </h3>
+                  <p>{d.g_day} 발행</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
 
-      <div className="guide-modal">
-            {/* {props.data.map((data, i) => {
+        <div className="guide-modal">
+          {/* {props.data.map((data, i) => {
               return (
                 <div key={i}>
                   <div>{data.g_no}</div>
@@ -98,26 +106,23 @@ function List(props) {
                 </div>
               );
             })}  */}
-            {
-              modal === true
-              ? <ModalPage 제목={제목} 누른제목={누른제목}></ModalPage>
-              : null
-            }
+          {modal === true ? (
+            <ModalPage 제목={제목} 누른제목={누른제목}></ModalPage>
+          ) : null}
+        </div>
       </div>
-</div>
     </div>
   );
 }
 
-  function ModalPage(props) {
-    return (
-      <div>
-        <h2>{props.제목[props.누른제목]}</h2>
-        <p>날짜</p>
-        <p>내용</p>
-      </div>
-    )
-  }
-
+function ModalPage(props) {
+  return (
+    <div>
+      <h2>{props.제목[props.누른제목]}</h2>
+      <p>날짜</p>
+      <p>내용</p>
+    </div>
+  );
+}
 
 export default List;
