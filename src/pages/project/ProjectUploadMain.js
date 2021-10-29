01a2a1cfbce4349536e3068ca850f0cea6af961c
jihye <wlgp1056@naver.com>
2021-10-29 21:47:18 +0900

    jh_1029_프로젝트업로드메인페이지

---
+++ src/pages/project/ProjectUploadMain.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainTab from "./MainTab";
+
+function ProjectUploadMain(props) {
+  return (
+    <div className="body">
+      <MainTab />
+    </div>
+  );
+}
+
+export default ProjectUploadMain;
