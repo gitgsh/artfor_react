a190faff8e97c3d330d992c2bdf27fb19f6d25db
bitcoinbucket <googoojoa@gmail.com>
2021-10-30 22:04:51 +0900

    dddd

--- src/pages/project/FundingPlan.js
+++ src/pages/project/FundingPlan.js
@@ -5,11 +5,6 @@ import TextField from "@mui/material/TextField";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import { LeftCircleFilled } from "@ant-design/icons";
-import { display, width } from "@mui/system";
 
 function FundingPlan(props) {
   const [value, setValue] = React.useState(new Date());
