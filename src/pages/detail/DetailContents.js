086b9183a4161ed71ca0286f7c856e3c367228dc
jihye <wlgp1056@naver.com>
2021-11-17 11:26:06 +0900

    jh_1117

--- src/pages/detail/DetailContents.js
+++ src/pages/detail/DetailContents.js
@@ -14,7 +14,7 @@ function DetailContents(props) {
   const { mainStore } = props;
   const { works, work } = mainStore;
   const findFunding = works.find(function (result) {
-    //사용자가 요청한 seq값과 일치하는 seq(db상의 seq값)을 찾는다..
+    //사용자가 요청한 seq값과 일치하는 seq(db상의 seq값)을 찾는다...
     return result.work_no == no;
   });
 
