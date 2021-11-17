5c04f4fb51436f37edd3dd0c7c3d13a6c4c21db3
gitgsh <googoojoa@gmail.com>
2021-11-17 17:27:37 +0900

    병합충돌2

---
+++ src/pages/users/MyProject/LikeProject.js
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import { Card, CardGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "../Project.css";
+import { inject, observer } from "mobx-react";
+import { height } from "@mui/system";
+import { useParams } from "react-router";
+
+function LikeProject(props) {
+  const { no } = useParams();
+  const { mainStore } = props;
+  const { works, work } = mainStore;
+
+  const { likeStore } = props;
+  const { likes, like } = likeStore;
+
+  const [show, setShow] = useState(false);
+  const count = work.length; // 총 프로젝트 갯수
+
+  //  window.localStorage.getItem("name");
+
+  const [mainWorks, setMainWorks] = useState(null);
+  const [likeProject, setLikeProject] = useState(null);
+  useEffect(() => {
+    let isComponentMounted = true;
+    const fetchData = async () => {
+      if (isComponentMounted) {
+        setMainWorks(mainStore.worksRead());
+        setLikeProject(likeStore.likesRead());
+      }
+    };
+    fetchData();
+    return () => {
+      isComponentMounted = false;
+    };
+  }, []);
+  console.log("email", window.localStorage.getItem("email"));
+  console.log("works>>>>>", works);
+  const findLikeProject = likes.filter(function (result) {
+    //사용자가 요청한 seq값과 일치하는 seq(db상의 seq값)을 찾는다.
+    return result.user_email == window.localStorage.getItem("user_email");
+  });
+  console.log("findLikeProject", findLikeProject);
+
+  const findWorkNo = findLikeProject.map(function (data, i) {
+    return data.work_no;
+  });
+
+  const workList = works.filter(function (data, i) {
+    // console.log("findWorkNO[i]", parseInt(findWorkNO[i]));
+
+    return findWorkNo.includes();
+  });
+
+  console.log("workList", workList);
+
+  return (
+    <div>
+      <div className="project_component" style={{ height: "100%" }}>
+        <div>
+          <CardGroup className="cardgroup_Home" style={{ height: "auto" }}>
+            {workList.slice(0, count).map(function (data, i) {
+              //8개
+              return (
+                <div
+                  style={{
+                    marginLeft: "10px",
+                    marginBottom: "-40px",
+                  }}
+                >
+                  <Card className="card_Home">
+                    <Link
+                      to={`/detail/${data.work_no}`}
+                      onClick={() => {
+                        mainStore.getWork(data.work_no);
+                        setShow(false);
+                      }}
+                    >
+                      <Card.Img
+                        variant="top"
+                        src={`/image/${data.work_img}`}
+                        style={{
+                          width: "250px",
+                          height: "200px",
+                          borderRadius: 15,
+                          overflow: "hidden",
+                        }}
+                      />
+                    </Link>
+                    <Card.Body
+                      className="cardbody_Home"
+                      style={{
+                        maxWidth: "250px",
+                        textAlign: "left",
+                      }}
+                    >
+                      <Card.Title className="cardtitle_Home">
+                        예술 | {data.artist_name}
+                      </Card.Title>
+                      <Card.Text
+                        className="cardtext_Home"
+                        style={{ minHeight: "80px" }}
+                      >
+                        <Link
+                          to={`/detail/${data.work_no}`}
+                          onClick={() => {
+                            mainStore.getWork(data.work_no);
+                            setShow(false);
+                          }}
+                        >
+                          {data.work_title}
+                        </Link>
+                      </Card.Text>
+                      <div
+                        className="cardfooter"
+                        style={{
+                          fontSize: "18px",
+                          textAlign: "center",
+                        }}
+                      >
+                        <p style={{ marginTop: "-35px" }}>
+                          {Math.round(
+                            (data.funding_now / data.funding_goal) * 100
+                          )}
+                          % 달성
+                        </p>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            })}
+          </CardGroup>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default inject("likeStore", "mainStore")(observer(LikeProject));
